2654e4ffa1108f556b0a372dbc220215b5b1409e
AchariNagaraj <acharinagaraj19@gmail.com>
2025-10-17 22:24:34 +0530

    input,output ,variables and datatypes

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Sum {//sum of a and b user input
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number");
+        int b=sc.nextInt();
+        System.out.println(a+b);
+
+        
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        //variables 
+        int a=10,b=5;
+        
+        int calc=a*b/(a-b);
+        System.out.println(calc);
+    }
+}
