3e8876b5c502b711adf4c299ce775a5e500eea2e
AchariNagaraj <acharinagaraj19@gmail.com>
2025-10-17 23:47:01 +0530

    conditionals

---
+++ Circlearea.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+
+public class Circlearea {
+   public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius");    
+        float rad=sc.nextFloat();
+        System.out.println("Area of circle is "+ (float)Math.PI*Math.pow(rad,2));
+   }
+
+
+
+}
---
+++ Table.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter no:");
+        int n=sc.nextInt();
+        System.out.println(n+"x"+1+"="+n);
+        System.out.println(n+"x"+2+"="+2*n);
+        System.out.println(n+"x"+3+"="+3*n);
+        System.out.println(n+"x"+4+"="+4*n);
+        System.out.println(n+"x"+5+"="+5*n);
+        System.out.println(n+"x"+6+"="+6*n);
+        System.out.println(n+"x"+7+"="+7*n);
+        System.out.println(n+"x"+8+"="+8*n);
+        System.out.println(n+"x"+9+"="+9*n);
+        System.out.println(n+"x"+10+"="+10*n);
+        
+    }
+}
