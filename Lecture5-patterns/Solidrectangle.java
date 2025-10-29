64aac27c200f686a9536359900fdbcba579223c4
AchariNagaraj <acharinagaraj19@gmail.com>
2025-10-29 13:06:38 +0530

    organizing everthing

---
+++ Lecture5-patterns/Solidrectangle.java
@@ -0,0 +1,14 @@
+//package Lecture5-patterns;
+//solid rectangle
+//r=4 c=5
+public class Solidrectangle {
+    public static void main(String[] args) {
+        int r=4,c=5;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
