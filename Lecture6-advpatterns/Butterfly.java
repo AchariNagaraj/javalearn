bf0eacc68e67101c02f308789e9a0f3ad72876f2
AchariNagaraj <acharinagaraj19@gmail.com>
2025-10-29 12:58:10 +0530

    butterfly and rhombus

---
+++ Lecture6-advpatterns/Butterfly.java
@@ -0,0 +1,33 @@
+//package Lecture6-advpatterns;
+//My approach
+public class Butterfly {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2*n;j++){
+                if(j<=i || j>2*n-i){
+                    System.out.print("*");
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=2*n;j++){
+                if(j<=i || j>2*n-i){
+                    System.out.print("*");
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
