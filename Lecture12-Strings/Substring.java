ed47fe9880f96405aab2f10029d1d5063d638a2a
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-12 16:51:17 +0530

    Substring using substring(begInd,endInd) method

---
+++ Lecture12-Strings/Substring.java
@@ -0,0 +1,7 @@
+public class Substring {
+    public static void main(String args[]){
+        String s1="Hello World";
+        System.out.println(s1.substring(4,7));
+        //endIndex will print till 6 (7-1)
+    }
+}
