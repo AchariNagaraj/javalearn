54d9856d1d695fd31fb8d24baeb52fcce5d17e6b
AchariNagaraj <acharinagaraj19@gmail.com>
2026-01-24 21:42:07 +0530

    learnt StringBuilder intro

---
+++ Lecture13-StringBuilder/LearningTheClass.java
@@ -0,0 +1,9 @@
+// package Lecture13-StringBuilder;
+
+public class LearningTheClass {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Tony");
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+    }
+}
